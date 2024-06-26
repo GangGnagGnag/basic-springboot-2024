f50cace09512594fcd7ca36324ed521f3e626714
GyeongHo <kungho84@gmail.com>
2024-06-26 17:41:04 +0900

    24.06.26 커밋

--- spring03/backboard/src/main/java/com/jkh9610/backboard/controller/MainController.java
+++ spring03/backboard/src/main/java/com/jkh9610/backboard/controller/MainController.java
@@ -14,13 +14,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class MainController {
     
     @GetMapping("/hello")
-    public String getHello() {
-        log.info("getHello(); 실행");
+    public String hello() {
+        log.info("getHello(); 실행.");
         return "hello";
     }
 
     @GetMapping("/")
-    public String getMain() {
-        return "redirect:/board/list"; //loaclhost8080/ -> localhost8080/board/list변경
+    public String main() {
+        return "redirect:/board/list/free"; // localhost:8080/ -> localhost:8080/board/list 변경
     }
 }
--- spring03/backboard/src/main/java/com/jkh9610/backboard/entity/Board.java
+++ spring03/backboard/src/main/java/com/jkh9610/backboard/entity/Board.java
@@ -25,36 +25,40 @@ import lombok.Setter;
 @Setter
 @Entity // 테이블화
 @Builder // 객체 생성을 간략화
-@NoArgsConstructor // 파라미터 없는 기본생성자 자동 생성
-@AllArgsConstructor //맴버변수 모두를 파라미터로 가지는 생성자 자동생성 
-public class Board {    
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE) // 나중에 Oracle로 바꿀거다 라는 의미
-    private Long bno;   // PK
-
-    @Column(name="title", length = 250)
-    private String title;   // 글제목
+@NoArgsConstructor // 파라미터 없는 기본생성자 자동생성
+@AllArgsConstructor // 멤버변수 모두를 파라미터로 가지는 생성자 자동생성
+public class Board {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE) // 나는 나중에 Oracle로 바꿀꺼야!
+    private Long bno; // PK
+
+    @Column(name = "title", length = 250)
+    private String title; // 글제목
+
     @Column(name = "content", length = 4000)
     private String content; // 글내용
 
     @CreatedDate
     @Column(name = "createDate", updatable = false)
-    private LocalDateTime createDate; // 글 생성일
+    private LocalDateTime createDate; // 글생성일
 
     @LastModifiedDate
-    @Column(name = "modifyDate")// updatable = true 가 기본 값이 기때문에 따로 적지 않아도 됨
-    private LocalDateTime modifyDate; // 글 수정일 기능추가 (24.06.24추가) 
+    @Column(name = "modifyDate")
+    private LocalDateTime modifyDate; // 24.06.24 수정일 추가
 
-    // 사용자가 여러개의 게시글을 작성할 수 있다
+    // @ColumnDefault("0") // 생성될때 초기값 0이 들어감
+    // @Column(columnDefinition = "integer default 0")
+    private Integer hit; // 24.06.26 조회수 추가
+
+    // 사용자가 여러개의 게시글을 작성할 수 있다. 다대일 설정
     @ManyToOne
     private Member writer; 
 
-    // 중요!!! RelationShip 일대다
-    @OneToMany(mappedBy = "board", cascade = CascadeType.REMOVE)    // 게시글을 지우게 되면 게시글에 관련된 댓글도 지운다는 의미
-    private List<Reply> replyList; // 댓글 리스트
+    // 중요, RelationShip 일대다 설정
+    @OneToMany(mappedBy = "board", cascade = CascadeType.REMOVE)
+    private List<Reply> replyList;
 
     @ManyToOne
-    private Category category;  //free, qna 로 구분해서 글생성 가능
+    private Category category; // free, qna로 구분해서 글생성 가능
 }
